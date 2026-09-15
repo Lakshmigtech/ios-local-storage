c29a072463ad2236e867a2badf2d8c7480cb8c1a
Lakshmi <98391736+Lakshmigtech@users.noreply.github.com>
2026-09-15 22:29:17 +0530

    Add keychain save functionality for access token

--- Security/SecureStorageKey.swift
+++ Security/SecureStorageKey.swift
@@ -5,3 +5,7 @@ enum SecureStorageKey {
     static let accessToken = "access_token"
     static let refreshToken = "refresh_token"
 }
+try keychain.save(
+    token,
+    forKey: SecureStorageKey.accessToken
+)
